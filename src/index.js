5098fa28a5eabf85a3637edd28efd52591f93fe7
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-01-08 20:12:23 +0530

    supratim531: index.js refactored

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
 import './index.css';
-import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
